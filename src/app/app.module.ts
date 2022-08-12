dff5589c27b91a4510a2243c2e4bc152a49f2c44
Willian Lira <will.lirag3@gmail.com>
2022-08-12 18:33:23 -0300

    feat(banner): banner has been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, WrapperComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/wrapper/wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wrapper',
+  templateUrl: './wrapper.component.html',
+  styleUrls: ['./wrapper.component.css'],
+})
+export class WrapperComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

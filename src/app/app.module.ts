ee83f8d8a09f1afa5f6ae512d98a235cab4e3538
Willian Lira <will.lirag3@gmail.com>
2022-08-17 11:10:05 -0300

    feat(intro): component has been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BannerComponent, WrapperComponent],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, WrapperComponent, IntroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
